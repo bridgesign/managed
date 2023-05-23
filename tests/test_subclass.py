ae7acde709a06a7e2b3fa72976374be8527a32f8
bridgesign <eyeoeternity@gmail.com>
2023-05-23 11:21:56 -0700

    Corrections in tests

--- tests/test_subclass.py
+++ tests/test_subclass.py
@@ -5,33 +5,33 @@ from managed import ManagedTensor as mt, device_manager as dm
 class TestManagedMethods(unittest.TestCase):
     def test_cuda(self):
         a = torch.rand(3).as_subclass(mt)
-        self.assertEqual(a.device, dm.cpu)
+        self.assertEqual(a.device, dm.cpu_device)
         a = a.cuda()
-        self.assertEqual(a.device, dm.gpu)
+        self.assertTrue(a.device in dm.cuda_devices)
         self.assertEqual(a.__class__, mt)
     
     def test_add(self):
         a = torch.rand(3).as_subclass(mt)
         b = torch.rand(3).as_subclass(mt)
         c = a + b
-        self.assertEqual(c.device, dm.cpu)
+        self.assertEqual(c.device, dm.cpu_device)
         self.assertEqual(c.__class__, mt)
         a = a.cuda()
         b = b.cuda()
         c = a + b
-        self.assertEqual(c.device, dm.gpu)
+        self.assertTrue(c.device in dm.cuda_devices)
         self.assertEqual(c.__class__, mt)
     
     def test_stack(self):
         a = torch.rand(3).as_subclass(mt)
         b = torch.rand(3).as_subclass(mt)
         c = torch.stack([a, b])
-        self.assertEqual(c.device, dm.cpu)
+        self.assertEqual(c.device, dm.cpu_device)
         self.assertEqual(c.__class__, mt)
         a = a.cuda()
         b = b.cuda()
         c = torch.stack([a, b])
-        self.assertEqual(c.device, dm.gpu)
+        self.assertTrue(c.device in dm.cuda_devices)
         self.assertEqual(c.__class__, mt)
 
 if __name__ == '__main__':
