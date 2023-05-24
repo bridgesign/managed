261d5b1be2b196bae7aa2a3a2feaf1dc583acd56
bridgesign <eyeoeternity@gmail.com>
2023-05-24 15:16:09 -0700

    Test this

--- managed/tensor.py
+++ managed/tensor.py
@@ -73,9 +73,10 @@ def hook_fn(grad_fn):
                 continue
             if device == None:
                 if hasattr(grad_fn, "variable"):
-                    device = grad_fn.variable.device
-                else:
                     device = grad_fn.metadata["device"]
+                else:
+                    continue
+                    # device = grad_fn.metadata["device"]
             if grad.device != device:
                 grad.data = grad.data.to(device)
         return grad_list
