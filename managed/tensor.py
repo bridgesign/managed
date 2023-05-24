e85e6d2687cd45c0c758a66de94297a11546ef3a
bridgesign <eyeoeternity@gmail.com>
2023-05-24 07:20:52 -0700

    Back tensor hook

--- managed/tensor.py
+++ managed/tensor.py
@@ -8,7 +8,7 @@ FUNC_BLACKLIST = (
     "__get__", "__set__", "__del__",
     "numel", "element_size", "to", "pinned",
     "__repr__", "register_hook", "register_backward_hook",
-    "is_leaf", "is_pinned", "is_contiguous",
+    "_magic_handle", "is_leaf", "is_pinned", "is_contiguous",
     "is_nonzero", "is_same_size", "is_set_to", "is_signed",
     "is_storage", "is_uninitialized", "is_variable",
     "is_cuda", "is_sparse", "is_quantized", "is_meta",
@@ -63,10 +63,6 @@ def hook_fn(grad_fn):
     device_list = [extract_device(gf[0]) for gf in grad_fn.next_functions]
     def func(grad_list):
         if len(grad_list) != len(device_list):
-            print(f"Hooked {grad_fn.name()} on {grad_fn.metadata['device']}")
-            for grad in grad_list:
-                if grad.data.device != grad_fn.metadata["device"]:
-                    grad.data = grad.data.to(grad_fn.metadata["device"])
             return grad_list
         print(f"Hooked {grad_fn.name()} on {device_list}")
         for grad, device in zip(grad_list, device_list):
@@ -75,6 +71,14 @@ def hook_fn(grad_fn):
             if grad.data.device != device:
                 grad.data = grad.data.to(device)
         return grad_list
+    return func
+
+def tensor_hook_fn(tensor):
+    def func(grad):
+        if grad.data.device != tensor.device:
+            grad.data = grad.data.to(tensor.device)
+        tensor._magic_handle.remove()
+        return grad
     return func
 
 class ManagedTensor(_ManagedTensor):
@@ -96,6 +100,11 @@ class ManagedTensor(_ManagedTensor):
         # Issue: https://github.com/pytorch/pytorch/issues/65016
         # Remove this when issue is fixed
         ##############################
+        if func.__name__ == "backward":
+            for tensor in tensor_list:
+                if tensor.requires_grad and tensor.is_leaf:
+                    print(f"Hooking {tensor} - {tensor.device}")
+                    tensor._magic_handle = tensor.register_hook(tensor_hook_fn(tensor))
         if func.__name__ not in FUNC_BLACKLIST and func.__name__ != "backward":
             ret_list = []
             aggregate_tensors(ret_list, ret)
