3b2c6d056487a491c9ce33aace1279b6afaa1b7e
bridgesign <eyeoeternity@gmail.com>
2023-07-10 11:16:36 -0700

    setup changed

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 if __name__ == '__main__':
     setup(
         name='tensor-managed',
-        version='0.1',
+        version='0.1.1',
         description='Automated GPU Allocation for PyTorch',
         long_description=open('README.md').read(),
         long_description_content_type='text/markdown',
@@ -13,7 +13,7 @@ if __name__ == '__main__':
         author_email="eyeoeternity@gmail.com",
         url="https://github.com/bridgesign/managed",
         packages=find_packages(exclude=('tests',)),
-        license='MIT',
+        license='Apache License 2.0',
         install_requires=[
             'torch',
         ],
