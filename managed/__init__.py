46d179d8b809d990aa12c90d0c337025ee85d202
bridgesign <eyeoeternity@gmail.com>
2023-08-09 14:05:02 -0700

    Add manage flag

--- managed/__init__.py
+++ managed/__init__.py
@@ -1,8 +1,15 @@
 from .device_manager import DeviceManager, USE_HEURISTIC, HEUSRISTIC_FUNCTION
 device_manager = DeviceManager()
-from .tensor import ManagedTensor
-from torch import Tensor
-from .module import ManagedModule
+
+import os
+
+if os.environ.get("NO_MANAGED", "0") == "1":
+    import torch
+    ManagedTensor = torch.Tensor
+    ManagedModule = torch.nn.Module
+else:
+    from .tensor import ManagedTensor
+    from .module import ManagedModule
 
 __all__ = [
     "USE_HEURISTIC",
